056444e24c1429bbe632f0807b577858f85e800a
furkanyucel <furkanyucelux@gmail.com>
2024-07-03 11:06:24 +0300

    İlgili kodlar ve veri olmadığı zaman sonuç tablosunun gizliyen kodlar eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import './App.css';
+import { useState } from "react";
+import AddWorker from "./components/AddWorker";
+import WorkerList from "./components/WorkerList";
+
+import "./App.css";
 
 function App() {
-  return <></>;
+  const [allData, setAllData] = useState([]);
+
+  const users = (enteredName, enteredWage) => {
+    const newUsers = {
+      id: Math.random().toString(),
+      name: enteredName,
+      wage: enteredWage,
+    };
+
+    setAllData((prevState) => [...prevState, newUsers]);
+  };
+
+  return (
+    <>
+      <h1>Personel Maaş Tablosu</h1>
+      <AddWorker onUsers={users} />
+      {allData.length > 0 && (
+        <WorkerList allData={allData} setAllData={setAllData} />
+      )}
+    </>
+  );
 }
 
 export default App;
